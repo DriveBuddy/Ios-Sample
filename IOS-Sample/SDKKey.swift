e5077a97bb55051b259d07e9c925b2d029a6d30b
Caglar Uslu <caglaruslu12@gmail.com>
2018-12-20 00:00:23 +0300

    initial commit

---
+++ IOS-Sample/SDKKey.swift
@@ -0,0 +1,11 @@
+//
+//  SDKKey.swift
+//  DriveBuddyTestProject
+//
+//  Created by Çağlar Uslu on 30.10.2018.
+//  Copyright © 2018 Çağlar Uslu. All rights reserved.
+//
+
+import Foundation
+
+let SDK_KEY = "your_sdk_key"
---
+++ Pods/Target Support Files/DriveBuddySDK/DriveBuddySDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DriveBuddySDK : NSObject
+@end
+@implementation PodsDummy_DriveBuddySDK
+@end
